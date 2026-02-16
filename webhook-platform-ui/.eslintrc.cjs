574cfa03ed42349e23efabd3ab3e86767f549671
vadymkykalo <vadymkykalo@gmail.com>
2026-02-16 15:00:02 +0200

    fix: relax ESLint rules for existing codebase

--- webhook-platform-ui/.eslintrc.cjs
+++ webhook-platform-ui/.eslintrc.cjs
@@ -25,7 +25,9 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
